0d6c7143e42fd086b2f7b6dbe7ccc6b91fdef830
James Anderson <me@jamesaanderson.com>
2014-05-05 19:01:42 -0700

    Add canvas

--- src/cell/core.cljs
+++ src/cell/core.cljs
@@ -1,3 +1,11 @@
 (ns cell.core)
 
-(. js/console (log "Hello world!"))
+(def width (.-innerWidth js/window))
+(def height (.-innerHeight js/window))
+
+(defn main []
+  (def canvas (.createElement js/document "canvas"))
+  (def ctx (.getContext canvas "2d"))
+  (.appendChild js/document.body canvas))
+
+(main)
